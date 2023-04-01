a4303398b2a891b2b9383d79b5ef56d38ff70ffb
Lee Martin <hello@leejohnmartin.co.uk>
2023-04-01 09:21:46 +0100

    Added: Crude but effective public serving

--- server/src/main.go
+++ server/src/main.go
@@ -8,9 +8,34 @@ import (
 	"os"
 )
 
-func getRoot(w http.ResponseWriter, r *http.Request) {
-	fmt.Printf("got / request\n")
-	io.WriteString(w, "This is my website!\n")
+func serveFile(w http.ResponseWriter, r *http.Request, f *os.File) {
+	fileInfo, err := f.Stat()
+	if err != nil {
+		http.Error(w, "Error reading file", http.StatusInternalServerError)
+		return
+	}
+	http.ServeContent(w, r, fileInfo.Name(), fileInfo.ModTime(), f)
+}
+
+func getPublicFile(w http.ResponseWriter, r *http.Request) {
+	filepath := r.URL.Path
+	if filepath == "/" {
+		filepath = "index.html"
+	}
+	file, err := os.Open("./public" + filepath)
+	if err != nil {
+		indexFile, indexerr := os.Open("./public/index.html")
+		if indexerr != nil {
+			fmt.Printf("Catastrophic Error: Could not get base index fallback: %s\n", indexerr)
+			http.Error(w, "Error reading file", http.StatusInternalServerError)
+			os.Exit(1)
+		}
+		defer indexFile.Close()
+		serveFile(w, r, indexFile)
+		return
+	}
+	defer file.Close()
+	serveFile(w, r, file)
 }
 
 func getHello(w http.ResponseWriter, r *http.Request) {
@@ -23,8 +48,8 @@ func main() {
 
 	mux.HandleFunc("/api/hello", getHello)
 
-	mux.HandleFunc("/", getRoot)
-	mux.HandleFunc("*", getRoot)
+	mux.HandleFunc("/", getPublicFile)
+	mux.HandleFunc("*", getPublicFile)
 
 	err := http.ListenAndServe(":3333", mux)
 	if errors.Is(err, http.ErrServerClosed) {
