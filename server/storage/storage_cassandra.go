8112b30cf00f0de0a76ebc24cb392118531810db
Lee Martin <hello@leejohnmartin.co.uk>
2023-04-22 16:26:40 +0100

    WIP: Creating needed cass keyspace and tables

--- server/storage/storage_cassandra.go
+++ server/storage/storage_cassandra.go
@@ -2,11 +2,14 @@ package storage
 
 import (
 	"errors"
+	"time"
 
+	"github.com/gocql/gocql"
 	"github.com/google/uuid"
 )
 
 type CassandraActivityStorage struct {
+	Cluster *gocql.ClusterConfig
 }
 
 func (stg CassandraActivityStorage) Create(activity Activity) (Activity, error) {
@@ -34,6 +37,7 @@ func (stg CassandraActivityStorage) DeleteForPlan(id uuid.UUID) error {
 }
 
 type CassandraPlanStorage struct {
+	Cluster *gocql.ClusterConfig
 }
 
 func (stg CassandraPlanStorage) Create(plan Plan) (Plan, error) {
@@ -57,5 +61,61 @@ func (stg CassandraPlanStorage) Delete(id uuid.UUID) error {
 }
 
 func getCassandratorageClient() (Storage, error) {
-	return Storage{}, errors.New("Not Implemented")
+	cluster := gocql.NewCluster("localhost:9042")
+	cluster.Authenticator = gocql.PasswordAuthenticator{Username: "cassandra", Password: "cassandra"}
+	cluster.Consistency = gocql.Quorum
+	cluster.ProtoVersion = 4
+	cluster.ConnectTimeout = time.Second * 10
+	session, err := cluster.CreateSession()
+	if err != nil {
+		return Storage{}, errors.New("Cassandra Connection Error")
+	}
+	defer session.Close()
+
+	// Migrations
+	err = session.Query("CREATE KEYSPACE IF NOT EXISTS ohs_planner WITH REPLICATION = {'class' : 'SimpleStrategy', 'replication_factor' : 3};").Exec()
+	if err != nil {
+		return Storage{}, errors.New("Error creating keyspace")
+	}
+
+	err = session.Query(`CREATE TABLE IF NOT EXISTS ohs_planner.activities (
+		userId text,
+		id UUID,
+		planId UUID,
+		summary text,
+		stages text,
+		dateTime timestamp,
+		timeRelevant boolean,
+		completed boolean,
+		notes text,
+		PRIMARY KEY ((userId), id)
+	);`).Exec()
+
+	if err != nil {
+		return Storage{}, errors.New("Error creating activities table")
+	}
+
+	timeIndex := session.Query("CREATE INDEX IF NOT EXISTS ON ohs_planner.activities (dateTime);").Exec()
+	planIndex := session.Query("CREATE INDEX IF NOT EXISTS ON ohs_planner.activities (planId);").Exec()
+
+	if timeIndex != nil || planIndex != nil {
+		return Storage{}, errors.New("Error creating activities indexes")
+	}
+
+	err = session.Query(`CREATE TABLE IF NOT EXISTS ohs_planner.plans (
+		userId text,
+		id UUID,
+		name text,
+		active boolean,
+		PRIMARY KEY ((userId), id)
+	);`).Exec()
+
+	if err != nil {
+		return Storage{}, errors.New("Error creating plans table")
+	}
+
+	return Storage{
+		Activity: CassandraActivityStorage{Cluster: cluster},
+		Plan:     CassandraPlanStorage{Cluster: cluster},
+	}, nil
 }
