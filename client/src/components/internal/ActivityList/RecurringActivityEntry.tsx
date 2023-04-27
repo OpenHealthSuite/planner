9ada6789cddcaf4c1dc9cef078a202d7a3894dd8
Lee Martin <hello@leejohnmartin.co.uk>
2023-04-27 22:47:41 +0100

    Added: View and Edit recurring

--- client/src/components/internal/ActivityList/RecurringActivityEntry.tsx
+++ client/src/components/internal/ActivityList/RecurringActivityEntry.tsx
@@ -1,14 +1,22 @@
-import { Flex, IconButton,Text } from "@chakra-ui/react";
+import { Button, Flex, IconButton,Modal,ModalCloseButton,ModalContent,ModalOverlay,Tab,TabList,TabPanel,TabPanels,Tabs,Text, useDisclosure } from "@chakra-ui/react";
 import { addDays, isAfter } from "date-fns";
-import { CheckIcon, RepeatIcon } from "@chakra-ui/icons";
-import { Activity, ActivityApiSubmission, RecurringActivity } from "../../../types";
+import { CheckIcon, RepeatIcon, ViewIcon } from "@chakra-ui/icons";
+import { Activity, ActivityApiSubmission, RecurringActivity, RecurringActivityApiSubmission } from "../../../types";
 import { useCallback, useState } from "react";
-import { plannerPostRequest } from "../../../utilities/apiRequest";
+import { plannerPostRequest, plannerPutRequest } from "../../../utilities/apiRequest";
+import { ActivityDetails } from "../../ActivityDetails";
+import { RecurringActivityForm } from "../../RecurringActivityEditor";
+
+const editRecurringActivitySubmission = (activity: RecurringActivityApiSubmission) => {
+  return plannerPutRequest<RecurringActivityApiSubmission, string>(`/recurring_activities/${activity.id}`, activity);
+};
 
 export const RecurringActivitySummary = ({ daysActivities, activity, activityDay, onUpdate } : { daysActivities?: Activity[] ,activity: RecurringActivity, activityDay: Date, onUpdate: (str: string) => void }) => {
   const [loading, isLoading] = useState(false);
 
-  const createAssociatedCompleteActivity = useCallback((recurringActivity: RecurringActivity) => {
+  const { isOpen, onOpen, onClose } = useDisclosure();
+  
+  const createAssociatedActivity = useCallback((recurringActivity: RecurringActivity, completed: boolean) => {
     isLoading(true);
     const {summary, stages, timeRelevant} = recurringActivity;
     const activity: ActivityApiSubmission = {
@@ -17,10 +25,10 @@ export const RecurringActivitySummary = ({ daysActivities, activity, activityDay
       timeRelevant,
       recurringActivityId: recurringActivity.id,
       dateTime: activityDay.toISOString(),
-      completed: true,
-      notes: "Completed from Recurring Activity"
+      completed,
+      notes: "From Recurring Activity"
     };
-    plannerPostRequest<ActivityApiSubmission, string>("/activities", activity)
+    return plannerPostRequest<ActivityApiSubmission, string>("/activities", activity)
       .then(onUpdate)
       .finally(() => isLoading(false));
   }, [isLoading, onUpdate]);
@@ -41,8 +49,38 @@ export const RecurringActivitySummary = ({ daysActivities, activity, activityDay
       aria-label="Mark Done"
       marginLeft={"auto"}
       marginRight={0}
-      onClick={() => createAssociatedCompleteActivity(activity)}
+      onClick={() => createAssociatedActivity(activity, true)}
       isDisabled={loading}
       icon={<CheckIcon />} />}
+    <IconButton size="sm"
+      aria-label="View"
+      marginLeft={!isCompletable ? "auto" : "0.5em"} 
+      marginRight={0}
+      onClick={onOpen}
+      icon={<ViewIcon />} />
+    <Modal isOpen={isOpen} onClose={onClose}>
+      <ModalOverlay />
+      <ModalContent>
+        <ModalCloseButton />
+        <Tabs>
+          <TabList>
+            <Tab>View</Tab>
+            <Tab>Edit Recurring</Tab>
+          </TabList>
+          <TabPanels>
+            <TabPanel>
+              <ActivityDetails activity={activity}/>
+              <Button onClick={() => createAssociatedActivity(activity, false).then(onClose)}>Create Related Activity</Button>
+            </TabPanel>
+            <TabPanel>
+              <RecurringActivityForm activitySubmission={editRecurringActivitySubmission}
+                onCreated={onUpdate}
+                onClose={() => { return; }}
+                initialRecurringActivity={{...activity, date: activity.dateTimeStart.toISOString().split("T")[0]}}/>
+            </TabPanel>
+          </TabPanels>
+        </Tabs>
+      </ModalContent>
+    </Modal>
   </Flex>;
 };
