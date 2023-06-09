3a6b353b677e2bb46cb522505885626b8ca2baad
Lee Martin <hello@leejohnmartin.co.uk>
2023-06-09 21:52:46 +0100

    added: Modal for plan editing

--- client/src/components/PlanEditor.tsx
+++ client/src/components/PlanEditor.tsx
@@ -1,6 +1,13 @@
-import { IconButton } from "@chakra-ui/react";
+import { IconButton, Modal, ModalBody, ModalCloseButton, ModalContent, ModalHeader, ModalOverlay, useDisclosure } from "@chakra-ui/react";
 import { EditIcon } from "@chakra-ui/icons";
 import { Plan } from "../types";
+
+type PlanEditorFormProps = {
+  plan: Plan,
+  editPlan: (plan: Plan) => void,
+  deletePlan: (planId: string) => void,
+  onClose: () => void
+};
 
 type PlanEditorProps = {
   plan: Plan,
@@ -9,10 +16,28 @@ type PlanEditorProps = {
   styleProps: { [key: string]: string | number }
 };
 
+export const PlanEditorForm = ({ plan, editPlan, deletePlan, onClose }: PlanEditorFormProps) => {
+  return <>Form</>;
+};
+
 export const PlanEditor = ({ plan, editPlan, deletePlan, styleProps }: PlanEditorProps) => {
-  return <IconButton aria-label="Edit"
+  const { isOpen, onOpen, onClose } = useDisclosure();
+  return <><IconButton aria-label="Edit"
     {...styleProps}
     icon={<EditIcon />}
-    onClick={() => deletePlan(plan.id)}
-  />;
+    onClick={onOpen}
+  />
+  <Modal isOpen={isOpen} onClose={onClose}>
+    <ModalOverlay />
+    <ModalContent>
+      <ModalHeader>Edit plan</ModalHeader>
+      <ModalCloseButton />
+      <ModalBody>
+        <PlanEditorForm plan={plan}
+          editPlan={editPlan}
+          deletePlan={deletePlan}
+          onClose={onClose} />
+      </ModalBody>
+    </ModalContent>
+  </Modal></>;
 };
