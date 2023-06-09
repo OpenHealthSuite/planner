752ad1aa22daa99a0a69569b3e1172e62b3080ae
Lee Martin <hello@leejohnmartin.co.uk>
2023-06-09 22:19:23 +0100

    added: Very crude edit interface

--- client/src/components/PlanEditor.tsx
+++ client/src/components/PlanEditor.tsx
@@ -1,6 +1,7 @@
-import { IconButton, Modal, ModalBody, ModalCloseButton, ModalContent, ModalHeader, ModalOverlay, useDisclosure } from "@chakra-ui/react";
+import { Button, FormControl, FormLabel, IconButton, Input, Modal, ModalBody, ModalCloseButton, ModalContent, ModalFooter, ModalHeader, ModalOverlay, useDisclosure } from "@chakra-ui/react";
 import { EditIcon } from "@chakra-ui/icons";
 import { Plan } from "../types";
+import { Formik } from "formik";
 
 type PlanEditorFormProps = {
   plan: Plan,
@@ -17,7 +18,38 @@ type PlanEditorProps = {
 };
 
 export const PlanEditorForm = ({ plan, editPlan, deletePlan, onClose }: PlanEditorFormProps) => {
-  return <>Form</>;
+  return <Formik
+    initialValues={plan}
+    // validationSchema={RecurringActivitySchema}
+    onSubmit={async (values) => {
+      onClose();
+      editPlan(values);
+    }}
+  >
+    {({
+      values,
+      isValid,
+      dirty,
+      handleChange,
+      handleSubmit
+    }) => (
+      <form onSubmit={handleSubmit}>
+        <ModalBody>
+          <FormControl>
+            <FormLabel htmlFor={"name"}>Name</FormLabel>
+            <Input
+              id={"name"}
+              name={"name"}
+              onChange={handleChange} value={values.name}/>
+          </FormControl>
+        </ModalBody>
+        <ModalFooter>
+          <Button variant={"ghost"} onClick={onClose}>Cancel</Button>
+          <Button variant={"outline"} onClick={() => { deletePlan(plan.id); onClose(); }}>Delete</Button>
+          <Button type="submit" isDisabled={!dirty || !isValid}>Save</Button>
+        </ModalFooter>
+      </form>)}
+  </Formik>;
 };
 
 export const PlanEditor = ({ plan, editPlan, deletePlan, styleProps }: PlanEditorProps) => {
@@ -32,12 +64,10 @@ export const PlanEditor = ({ plan, editPlan, deletePlan, styleProps }: PlanEdito
     <ModalContent>
       <ModalHeader>Edit plan</ModalHeader>
       <ModalCloseButton />
-      <ModalBody>
-        <PlanEditorForm plan={plan}
-          editPlan={editPlan}
-          deletePlan={deletePlan}
-          onClose={onClose} />
-      </ModalBody>
+      <PlanEditorForm plan={plan}
+        editPlan={editPlan}
+        deletePlan={deletePlan}
+        onClose={onClose} />
     </ModalContent>
   </Modal></>;
 };
