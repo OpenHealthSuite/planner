23afcc688659bc88b0e87ff1b04a7c17f9014d6e
Lee Martin <hello@leejohnmartin.co.uk>
2023-06-10 15:03:58 +0100

    Added: Active checkbox

--- client/src/components/PlanEditor.tsx
+++ client/src/components/PlanEditor.tsx
@@ -1,7 +1,8 @@
-import { Button, FormControl, FormLabel, IconButton, Input, Modal, ModalBody, ModalCloseButton, ModalContent, ModalFooter, ModalHeader, ModalOverlay, useDisclosure } from "@chakra-ui/react";
+import { Button, Checkbox, FormControl, FormLabel, IconButton, Input, Modal, ModalBody, ModalCloseButton, ModalContent, ModalFooter, ModalHeader, ModalOverlay, useDisclosure } from "@chakra-ui/react";
 import { EditIcon } from "@chakra-ui/icons";
 import { Plan } from "../types";
 import { Formik } from "formik";
+import * as Yup from "yup";
 
 type PlanEditorFormProps = {
   plan: Plan,
@@ -17,10 +18,17 @@ type PlanEditorProps = {
   styleProps: { [key: string]: string | number }
 };
 
+const PlanSchema = Yup.object().shape({
+  name: Yup.string()
+    .min(1, "Needs at least one character")
+    .required("Required"),
+  active: Yup.boolean().required("Required")
+});
+
 export const PlanEditorForm = ({ plan, editPlan, deletePlan, onClose }: PlanEditorFormProps) => {
   return <Formik
     initialValues={plan}
-    // validationSchema={RecurringActivitySchema}
+    validationSchema={PlanSchema}
     onSubmit={async (values) => {
       onClose();
       editPlan(values);
@@ -41,6 +49,14 @@ export const PlanEditorForm = ({ plan, editPlan, deletePlan, onClose }: PlanEdit
               id={"name"}
               name={"name"}
               onChange={handleChange} value={values.name}/>
+          </FormControl>
+          <FormControl>
+            <Checkbox
+              id={"active"}
+              name={"active"}
+              onChange={handleChange} isChecked={values.active}>
+              Active
+            </Checkbox>
           </FormControl>
         </ModalBody>
         <ModalFooter>
