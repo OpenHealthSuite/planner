ac57cce18cadb39672b9257464cb547c06ac7398
Lee Martin <hello@leejohnmartin.co.uk>
2023-06-11 16:20:34 +0100

    added: Throw error on partial failure

--- client/src/utilities/activityParsing.test.ts
+++ client/src/utilities/activityParsing.test.ts
@@ -37,4 +37,61 @@ describe("parseActivityFromString", () => {
     expect(res.success).toBeTruthy();
     expect(res.success && res.activity).toEqual(activity);
   });
+
+  test.each(
+    [
+      [
+        "Some summary,2023-12-12T00:00:00,false,false,::3::Stage One,||15||min,::5::Stage Two,||20||miles,||10||km",
+        {
+          summary: "Some summary",
+          dateTime: parseISO("2023-12-12T00:00:00"),
+          timeRelevant: false,
+          completed: false,
+          stages: [
+            {
+              order: 0,
+              description: "Stage One",
+              repetitions: 3,
+              metrics: [
+                {
+                  amount: 15,
+                  unit: "min"
+                }
+              ]
+            },
+            {
+              order: 1,
+              description: "Stage Two",
+              repetitions: 5,
+              metrics: [
+                {
+                  amount: 20,
+                  unit: "miles"
+                },
+                {
+                  amount: 10,
+                  unit: "km"
+                }
+              ]
+            }
+          ]
+        }
+      ]
+    ]
+  )("Activities with stages :: parse successfully", (strActivity, activity) => {
+    const res = parseActivityFromString(strActivity);
+    expect(res.success).toBeTruthy();
+    expect(res.success && res.activity).toEqual(activity);
+  });
+
+  test.each([
+    ["Some summary,2023-12-12T00:00:00,false,false,::3::Stage One,| |15||min", 6],
+    ["Some summary,2023-12-12T00:00:00,false,false,::3: :Stage One,||15||min", 5],
+    ["Some summary,2023-12-12T00:00:00,false,false,::asdm::Stage One,||15||min", 5],
+    ["Some summary,2023-12-12T00:00:00,false,false,::3::Stage One,||ktgm||min", 6]
+  ])("Malformed stage :: returns error", (str, errorPos) => {
+    const res = parseActivityFromString(str);
+    expect(res.success).toBeFalsy();
+    expect(!res.success && res.error).toBe("Malformed part :: Error parsing item in index " + errorPos);
+  });
 });
--- client/src/utilities/activityParsing.ts
+++ client/src/utilities/activityParsing.ts
@@ -3,7 +3,7 @@
 //   unit: string;
 // };
 
-import { Activity } from "../types";
+import { Activity, ActivityStage } from "../types";
 import { parseISO } from "date-fns";
 
 // export type ActivityStage = {
@@ -27,7 +27,7 @@ import { parseISO } from "date-fns";
 // };
 
 // summary, dateTime, timeRelevant, completed, ::stageDesc::reps, ||amount||metric
-// Some summary, 2023-12-12, false, false, ::Stage One::3, ||15||min, ||20||min, ||Stage Two||5, ||23||min
+// Some summary, 2023-12-12, false, false, ::3::Stage One, ||15||min, ||20||min, ||Stage Two||5, ||23||min
 
 type ParsedActivity = {
   success: true,
@@ -41,22 +41,62 @@ type ErrorParsingActivity = {
 
 type ActivityParsingResult = ParsedActivity | ErrorParsingActivity;
 
+const STAGES_OFFSET = 5;
+
+const stageParsingReducer = (accumulator: ActivityStage[], stgPrt: string, index: number): ActivityStage[] => {
+  const stageError = new Error("Error parsing item in index " + (index + STAGES_OFFSET));
+  if (stgPrt.startsWith("::")) {
+    const [, rawRepetitions, description] = stgPrt.split("::");
+    const repetitions = parseInt(rawRepetitions);
+    if (!rawRepetitions || isNaN(repetitions) || !description) {
+      throw stageError;
+    }
+    accumulator.push({
+      order: accumulator.length,
+      description,
+      repetitions,
+      metrics: []
+    });
+  } else if (stgPrt.startsWith("||")) {
+    const [, rawAmount, unit] = stgPrt.split("||");
+    const amount = parseInt(rawAmount);
+    if (!amount || isNaN(amount) || !unit) {
+      throw stageError;
+    }
+    accumulator[accumulator.length - 1].metrics.push({
+      amount,
+      unit
+    });
+  } else {
+    throw stageError;
+  }
+  return accumulator;
+};
+
 export const parseActivityFromString = (input: string): ActivityParsingResult => {
   const [summary, dateTime, timeRelevant, completed, ...stages] = input.split(",");
   if (summary && dateTime && timeRelevant && completed) {
-    return {
-      success: true,
-      activity: {
-        summary,
-        dateTime: parseISO(dateTime),
-        timeRelevant: timeRelevant === "true",
-        completed: completed === "true",
-        stages: []
-      }
-    };
+    try {
+      return {
+        success: true,
+        activity: {
+          summary,
+          dateTime: parseISO(dateTime),
+          timeRelevant: timeRelevant === "true",
+          completed: completed === "true",
+          stages: stages.reduce(stageParsingReducer, [])
+        }
+      };
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    } catch (ex: any) {
+      return {
+        success: false,
+        error: "Malformed part :: " + ex.message
+      };
+    }
   }
   return {
     success: false,
-    error: "Not Implemented"
+    error: "Missing minimum data"
   };
 };
